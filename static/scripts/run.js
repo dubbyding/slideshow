471581d4fc7251f8550f25288e7aa07b2c683023
Raj Maharjan <rmaharjan.rm90@gmail.com>
2022-06-13 23:49:17 +0545

    two diffrent transition add

--- static/scripts/run.js
+++ static/scripts/run.js
@@ -5,11 +5,12 @@ import { rtg } from './randomTimeGenerator.js';
 let runFunction = (listOfImages, numberOfCarousel) => {
 	let carousel = [];
 	let button = [];
+	let transition = ['normal', 'cyclic'];
 
 	for (let i = 0; i < numberOfCarousel; i++) {
 		carousel[i] = new Carousel(i + 1, listOfImages, rtg(), rtg());
 		carousel[i].appendImages();
-		carousel[i].transitionImage();
+		carousel[i].transitionImage(transition[i]);
 		button[i] = new Buttons(i);
 		button[i].addButton('left');
 		button[i].addButton('right');
